bef662ac1af1bb5676bf0615c029ec48e12acf0c
Mouaaz Sweid <mmsweid@gmail.com>
2024-11-23 15:59:10 +0300

    add product table

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'price' => fake()->randomFloat(2, 1, 100), // Generate a price between 1.00 and 100.00
+        ];
+    }
+}
