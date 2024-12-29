cdb1a17bcb547782550f5eb84b28898fdd843b8f
ken <ahmadsoud63@gmail.com>
2024-12-29 15:12:20 +0300

    add a method for adding orders

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\OrderRequest;
 use App\Http\Resources\OrderResource;
 use App\Models\Order;
 use Illuminate\Container\Attributes\Auth;
@@ -26,5 +27,27 @@ class OrderController extends Controller
         $user = $request->user();
         $orders = Order::where('user_id',$user->id);
         return new OrderResource($orders);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(OrderRequest $request)
+    {
+        $order = Order::create([
+            'store_id' => $request->store_id,
+            'customer_id' => $request->user()->id, // Authenticated user as customer
+            'status' => 'pending', // Default status
+        ]);
+
+        $totalPrice=0;
+        // Attach products with quantities
+        foreach ($request->products as $product) {
+            $order->products()->attach($product['id'], ['quantity' => $product['quantity']]);
+            $totalPrice=$totalPrice+$product->price;
+        }
+        $order->total_price=$totalPrice;
+
+        return new OrderResource($order);
     }
 }
