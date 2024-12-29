a1cd901f1f4911a6ce3e5c2dc649a6eef621489c
ken <ahmadsoud63@gmail.com>
2024-12-29 14:55:09 +0300

    add a get all orders method

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Resources\OrderResource;
 use App\Models\Order;
+use Illuminate\Container\Attributes\Auth;
 use Illuminate\Http\Request;
 
 class OrderController extends Controller
@@ -15,5 +16,15 @@ class OrderController extends Controller
     {
         $order = Order::findOrFail($id);
         return new OrderResource($order);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function Orders(Request $request)
+    {   
+        $user = $request->user();
+        $orders = Order::where('user_id',$user->id);
+        return new OrderResource($orders);
     }
 }
