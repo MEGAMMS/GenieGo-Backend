346145c56958d69f566c6ce604825642754e66de
ken <ahmadsoud63@gmail.com>
2024-12-29 13:11:54 +0300

    fix4 search service

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\SearchRequest;
+use App\Models\Tag;
 use App\Services\SearchService;
 use App\Traits\ApiResponses;
 use Illuminate\Http\Request;
@@ -10,7 +11,7 @@ use Illuminate\Http\Request;
 use function Laravel\Prompts\error;
 
 class SearchController extends Controller
-{   
+{
     use ApiResponses;
 
     protected $searchService;
@@ -28,9 +29,16 @@ class SearchController extends Controller
         $tags = $request->input('tags', []); // Array of tags
         $query = $request->input('query');  // Search query
 
+        // Fetch tags from the database
+        $tagModels = Tag::whereIn('name', $tags)->get();
 
-        $results = $this->searchService->search( $tags, $query);
+        if ($tagModels->count() !== count($tags)) {
+            // If some tags are invalid, return empty results
+            return $this->error('Invalid tags', 400);
+        }
 
-        return $this->ok('search results',$results);
+        $results = $this->searchService->search($tags, $query);
+
+        return $this->ok('search results', $results);
     }
 }
--- app/Services/SearchService.php
+++ app/Services/SearchService.php
@@ -6,24 +6,16 @@ use App\Http\Resources\ProductResource;
 use App\Http\Resources\StoreResource;
 use App\Models\Product;
 use App\Models\Store;
-use App\Models\Tag;
+
 
 class SearchService
-{   
+{
     /**
      * Search for Products and Stores that match ALL specified tags and query.
      */
     public function search(array $tags, $query)
     {
         $query = trim($query);
-
-        // Fetch tags from the database
-        $tagModels = Tag::whereIn('name', $tags)->get();
-
-        if ($tagModels->count() !== count($tags)) {
-            // If some tags are invalid, return empty results
-            return ' ';
-        }
 
         // Search Products matching all tags and query
         $productResults = Product::whereHas('tags', function ($query) use ($tags) {
