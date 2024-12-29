f4451c237c7921882aca5bbbce9429c858765306
ken <ahmadsoud63@gmail.com>
2024-12-29 12:40:48 +0300

    fix3 search request

--- app/Services/SearchService.php
+++ app/Services/SearchService.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 namespace App\Services;
 
@@ -8,64 +8,57 @@ use App\Models\Product;
 use App\Models\Store;
 use App\Models\Tag;
 
+use function Laravel\Prompts\error;
+
 class SearchService
-{
-     /**
+{   
+    /**
      * Search for Products and Stores that match ALL specified tags and query.
      */
     public function search(array $tags, $query)
     {
-        $results = collect();
-
         $query = trim($query);
 
-        //if (!empty($tags)) {
-            // Fetch tags from the database
-            $tagModels = Tag::whereIn('name', $tags)->get();
+        // Fetch tags from the database
+        $tagModels = Tag::whereIn('name', $tags)->get();
 
-            if ($tagModels->count() !== count($tags)) {
-                // If some tags are invalid, return empty results
-                return $results;
-            }
+        if ($tagModels->count() !== count($tags)) {
+            // If some tags are invalid, return empty results
+            return ' ';
+        }
 
-            // Search Products matching all tags and query
-            $productResults = Product::whereHas('tags', function ($query) use ($tags) {
-                $query->whereIn('name', $tags);
-            }, '=', count($tags))
+        // Search Products matching all tags and query
+        $productResults = Product::whereHas('tags', function ($query) use ($tags) {
+            $query->whereIn('name', $tags);
+        }, '=', count($tags))
             ->where(function ($queryBuilder) use ($query) {
                 $queryBuilder->WhereHas('translations', function ($translationQuery) use ($query) {
-                                $translationQuery->where('name', 'LIKE', "%$query%");
-                             });
-            })
-            ->with(['translations' => function ($translationQuery) use ($query) {
-                $translationQuery->where('name', 'LIKE', "%$query%"); 
-            }])
-            ->get();
-        
-            $productResources = ProductResource::collection($productResults);
-
-            $results = $results->merge($productResources);
-
-            // Search Stores matching all tags and query
-            $storeResults = Store::whereHas('tags', function ($query) use ($tags) {
-                $query->whereIn('name', $tags);
-            }, '=', count($tags))
-            ->where(function ($queryBuilder) use ($query) {
-                $queryBuilder->WhereHas('translations', function ($translationQuery) use ($query) {
-                                $translationQuery->where('name', 'LIKE', "%$query%");
-                             });
+                    $translationQuery->where('name', 'LIKE', "%$query%");
+                });
             })
             ->with(['translations' => function ($translationQuery) use ($query) {
                 $translationQuery->where('name', 'LIKE', "%$query%");
             }])
             ->get();
-        
-            $storeResources = StoreResource::collection($storeResults);
 
-            $results = $results->merge($storeResources);
-        
+        // Search Stores matching all tags and query
+        $storeResults = Store::whereHas('tags', function ($query) use ($tags) {
+            $query->whereIn('name', $tags);
+        }, '=', count($tags))
+            ->where(function ($queryBuilder) use ($query) {
+                $queryBuilder->WhereHas('translations', function ($translationQuery) use ($query) {
+                    $translationQuery->where('name', 'LIKE', "%$query%");
+                });
+            })
+            ->with(['translations' => function ($translationQuery) use ($query) {
+                $translationQuery->where('name', 'LIKE', "%$query%");
+            }])
+            ->get();
 
-        return $results;
+
+        return [
+            'products' => ProductResource::collection($productResults),
+            'stores' => StoreResource::collection($storeResults)
+        ];
     }
-
 }
