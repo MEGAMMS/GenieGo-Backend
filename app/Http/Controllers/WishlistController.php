52e8a74dee80e8403ef1df7d4f64dbd64fdb5d66
ken <ahmadsoud63@gmail.com>
2024-12-31 14:51:56 +0300

    add store method

--- app/Http/Controllers/WishlistController.php
+++ app/Http/Controllers/WishlistController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Http\Resources\ProductResource;
+use App\Traits\ApiResponses;
 use Illuminate\Http\Request;
 
 class WishlistController extends Controller
 {
+    use ApiResponses;
     /**
      * Display the specified resource.
      */
@@ -17,5 +19,22 @@ class WishlistController extends Controller
         $wishlist = $user->customer->wishlist()->get();
 
         return ProductResource::collection($wishlist);
+    }
+
+    public function store(Request $request,$productId)
+    {
+
+        $customer = $request->user()->customer;
+
+        // Check if the product already exists in the wishlist
+        if ($customer->customer->wishlist()->where('product_id', $productId)->exists()) {
+            return $this->error('Product is already in your wishlist', 400);
+        }
+
+        // Add product to wishlist
+        $customer->wishlist()->attach($productId);
+
+        return $this->ok('Product added to wishlist');
+        
     }
 }
