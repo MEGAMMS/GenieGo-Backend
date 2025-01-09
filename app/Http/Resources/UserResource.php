ff8f518c3d1a3c34e7ee0a8cbfa384c2930fe335
ken <ahmadsoud63@gmail.com>
2025-01-09 09:06:50 +0300

    add store_id to owner resource

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -14,17 +14,25 @@ class UserResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return [
-            'id' => $this->id,
-            'first_name' => $this->first_name,
-            'last_name' => $this->last_name,
-            'username' => $this->username,
-            'email' => $this->email,
-            'phone' => $this->phone,
-            'icon' => $this->icon,
-            'role' => $this->getRole(),
-            /* 'created_at' => $this->created_at, */
-            /* 'updated_at' => $this->updated_at, */
+        $additionalDetails = [];
+    
+    if ($this->getRole() === 'Owner') {
+        $additionalDetails = [
+            'store_id' => $this->store,
         ];
+    }
+
+    return array_merge([
+        'id' => $this->id,
+        'first_name' => $this->first_name,
+        'last_name' => $this->last_name,
+        'username' => $this->username,
+        'email' => $this->email,
+        'phone' => $this->phone,
+        'icon' => $this->icon,
+        'role' => $this->getRole(),
+        /* 'created_at' => $this->created_at, */
+        /* 'updated_at' => $this->updated_at, */
+    ], $additionalDetails);
     }
 }
